4dc93f0595c77b069fbf3d0e802649d315d9f29d
justdan111 <emmanuelngulube292@gmail.com>
2026-09-09 21:26:25 +0100

    Refactor import statements in FoodDeliveryActivity component

--- widget/widgets/FoodDeliveryActivity.tsx
+++ widget/widgets/FoodDeliveryActivity.tsx
@@ -11,9 +11,7 @@ import {
   lineLimit,
   minimumScaleFactor,
   opacity,
-  padding,
-  shapes,
-} from '@expo/ui/swift-ui/modifiers';
+  padding, shapes, } from '@expo/ui/swift-ui/modifiers';
 import { createLiveActivity, type LiveActivityEnvironment } from 'expo-widgets';
 import type { SFSymbol } from 'sf-symbols-typescript';
 
